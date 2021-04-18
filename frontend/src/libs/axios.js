e65f8967b438bf172dc2c099f8255b543dc0f666
Yeamin Hossain <yeaminhossain2@gmail.com>
2021-04-18 19:21:33 +0600

    aixos update

--- frontend/src/libs/axios.js
+++ frontend/src/libs/axios.js
@@ -6,8 +6,8 @@ import axios from 'axios'
 const axiosIns = axios.create({
   // You can add your headers here
   // ================================
-  baseURL: 'http://127.0.0.1:8000',
-  // baseURL: 'https://amarclient.com/',
+  // baseURL: 'http://127.0.0.1:8000',
+  baseURL: 'https://amarclient.com/',
   timeout: 70000,
   headers: { Accept: 'application/json' },
 })
